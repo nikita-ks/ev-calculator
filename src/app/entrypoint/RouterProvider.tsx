85a9b088bbfecebbfb4b92bc397b33bb37abd7d1
nikita <nikita.ksyil@evergen.energy>
2025-11-23 22:32:21 +0200

    d

--- src/app/entrypoint/RouterProvider.tsx
+++ src/app/entrypoint/RouterProvider.tsx
@@ -1,10 +1,13 @@
 import type { FC } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
 import { MainLayout } from "../layout/main";
 import { CalculatorRoute } from "../routes/calculator";
 
-const browserRouter = createBrowserRouter([
-  { Component: MainLayout, children: [CalculatorRoute] },
+const browserRouter = createHashRouter([
+  {
+    Component: MainLayout,
+    children: [CalculatorRoute],
+  },
 ]);
 
 export const BrowserRouter: FC = () => (
