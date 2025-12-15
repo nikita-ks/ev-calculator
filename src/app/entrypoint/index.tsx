cc7998738dbab2477b412fa5a9339eba9dc921d1
NK <pancxxx2@gmail.com>
2025-12-15 07:30:30 +0200

    add sdk

--- src/app/entrypoint/index.tsx
+++ src/app/entrypoint/index.tsx
@@ -1,6 +1,10 @@
-import type { FC } from "react";
+import { useEffect, type FC } from "react";
 import { BrowserRouter } from "./RouterProvider";
+import { init } from "@tma.js/sdk-react";
 
 export const Entrypoint: FC = () => {
+  useEffect(() => {
+    init();
+  }, []);
   return <BrowserRouter />;
 };
